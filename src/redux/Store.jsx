c865ccadcf18fd11b82ed781858db397e3f44955
lamnaourm <mohammed.lamnaour@gmail.com>
2023-11-17 16:20:14 +0100

    extension redux

--- src/redux/Store.jsx
+++ src/redux/Store.jsx
@@ -1,9 +1,10 @@
-import { createStore } from "redux";
+import { applyMiddleware, compose, createStore } from "redux";
 import TodoReducer from "./TodoReducer";
 
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(TodoReducer);
+const store = createStore(TodoReducer, composeEnhancers(applyMiddleware()))
 
 
 export default store;
